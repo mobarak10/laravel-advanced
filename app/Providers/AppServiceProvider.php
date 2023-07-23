dfad6ff88cc1a86c84dc3d20ac6d63f0f0042361
Mobarak Hossain <47492246+mobarak10@users.noreply.github.com>
2023-07-23 10:05:28 +0600

    service container example

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Billing\BankPaymentGateway;
+use App\Billing\CreditPaymentGateway;
+use App\Billing\PaymentGatewayContract;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->app->singleton(PaymentGatewayContract::class, function ($app) {
+            if (request()->has('credit')) {
+                return new CreditPaymentGateway('usd');
+            }
+            return new BankPaymentGateway('usd');
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
